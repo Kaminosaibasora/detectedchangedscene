64e4c29c88d9de1ad40dd2e2356e3adaeb8bd56a
lancelot <LMao-DEVPRO@github.com>
2024-04-18 11:02:10 +0200

    finition des todo; nouvelle étape : interface graphique et repassage à la vidéo

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,7 @@
 from engine.VideoTraitment import VideoTraitement
 
 vt = VideoTraitement("./file_in/test_video.mp4")
-vt.decompose_img(100)
-vt.compare_frame()
+# vt.decompose_img(100)
+vt.detected_scene()
+
+# TODO : interface graphique
--- src/engine/VideoTraitment.py
+++ src/engine/VideoTraitment.py
@@ -3,35 +3,51 @@ import os
 from PIL import Image
 import numpy as np
 from scipy import ndimage
+from progress.bar import Bar
 
 class VideoTraitement :
     video_path = ""
     temp_path = ""
     folder_out_path = ""
+    frame_change = [] # TODO : retourner pour obtenir un traitement vidéo ?
+    # TODO : ajouter une fonction de destruction qui vide le dossier temp
+    # TODO : fonction de lecture vidéo d'une scene ?
 
     def __init__(self, video_path, temp_path = "./temp/", folder_out_path = "./file_out/") -> None:
+        """_summary_
+        Initialise les différents chemins.
+        Args:
+            video_path (_type_): chemin vers la vidéo
+            temp_path (str, optional): chemin vers le dossier temporaire. Defaults to "./temp/".
+            folder_out_path (str, optional): chemin vers le dossier de sortie. Defaults to "./file_out/".
+
+        Raises:
+            Exception: _description_
+        """
+        if not os.path.exists(video_path):
+            print("Le fichier n'existe pas !")
+            raise Exception("Le fichier vidéo n'existe pas !")
         self.video_path      = video_path
         self.temp_path       = temp_path
         self.folder_out_path = folder_out_path
-        # TODO vérifier l'existance des dossiers et les créer si nécessaire
-        # TODO : vérifier l'existance de la vidéo
-        if not os.path.exists(video_path):
-            print("Le fichier n'existe pas !")
+        if not os.path.exists(self.temp_path):
+            os.mkdir(self.temp_path)
+        if not os.path.exists(self.folder_out_path):
+            os.mkdir(self.folder_out_path)
 
     def decompose_img(self, size):
         """_summary_
-        décompose la vidéo en image
+        Décompose la vidéo en images.
         Args:
             size (int): tailler des vignettes de comparaison
         """
         try :
-            print("START")
             cap = cv2.VideoCapture(self.video_path)
             print("lecture de la vidéo : " + self.video_path)
             if not cap.isOpened():
-                print("Erreur lors de l'ouverture de la vidéo.")
-                exit()
+                raise Exception("Erreur lors de l'ouverture de la vidéo.")
             frame_count = 0
+            bar = Bar('Décomposition des frames', max=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
             while True:
                 ret, frame = cap.read()
                 if not ret:
@@ -39,46 +55,63 @@ class VideoTraitement :
                 image_path = f"{self.temp_path}frame_"+self.rename_image(frame_count)+".jpg"
                 new_frame = cv2.resize(frame, (size, size))
                 cv2.imwrite(image_path, new_frame)
-                # print(f"Frame {frame_count} enregistrée")
                 frame_count += 1
+                bar.next()
+            bar.finish()
             cap.release()
             cv2.destroyAllWindows()
         except Exception as e:
             print("ERROR" + str(e))
 
-    def compare_frame(self):
+    def detected_scene(self, delta = 500000):
+        """_summary_
+        Détecte les changement de scènes et met à jour la variable frame_change
+        Args:
+            delta (int, optional): valeur de détection d'un changement de scène. Defaults to 500000.
+        """
         list_img = os.listdir(self.temp_path)
-        frame_change = [0]
-        # print(list_img)
+        self.frame_change = [0]
         base = 0
         image_base = Image.open(self.temp_path + list_img[base])
         flou_base = ndimage.gaussian_filter(np.asarray(image_base), sigma=10)
         cran = 1
+        bar = Bar('Détection des scènes', max=len(list_img))
+        bar.next()
         while cran < len(list_img) :
             image_compare = Image.open(self.temp_path + list_img[cran])
             flou_compare = ndimage.gaussian_filter(np.asarray(image_compare), sigma=10)
-            nb_lignes,nb_colonnes,nb_dim = np.asarray(image_base).shape
-            diff = 0
-            for line in range(nb_lignes):
-                for col in range(nb_colonnes):
-                    # print("compare :", flou_base[line][col], flou_compare[line][col])
-                    for dim in range(nb_dim):
-                        dif = 0
-                        if flou_base[line][col][dim] > flou_compare[line][col][dim] :
-                            dif = flou_base[line][col][dim] - flou_compare[line][col][dim]
-                        else : 
-                            dif = flou_compare[line][col][dim] - flou_base[line][col][dim]
-                        # if dif != 0 :
-                        #     print(dif, ":", dim, ":", flou_base[line][col], flou_compare[line][col])
-                        diff += dif
-            if diff > 500000 :
-                print("point de dif entre ", list_img[base], "et", list_img[cran], ":", diff)
-                print("NOUVELLE SCENE !")
-                frame_change += [cran]
+            diff = self.compare_frame(flou_base, flou_compare, np.asarray(image_base).shape)
+            if diff > delta :
+                # print("point de dif entre ", list_img[base], "et", list_img[cran], ":", diff)
+                self.frame_change += [cran]
             flou_base = flou_compare
             base = cran
             cran += 1
-        print(frame_change)
+            bar.next()
+        bar.finish()
+        print(self.frame_change)
+
+    def compare_frame(self, image_base, image_compare, shape):
+        """_summary_
+        Compare 2 frames ensembles.
+        Args:
+            image_base (_type_): _description_
+            image_compare (_type_): _description_
+            shape (_type_): dimension de la matrice image
+
+        Returns:
+            int: valeur de différence
+        """
+        nb_lignes,nb_colonnes,nb_dim = shape
+        dif = 0
+        for line in range(nb_lignes):
+                for col in range(nb_colonnes):
+                    for dim in range(nb_dim):
+                        if image_base[line][col][dim] > image_compare[line][col][dim] :
+                            dif += image_base[line][col][dim] - image_compare[line][col][dim]
+                        else : 
+                            dif += image_compare[line][col][dim] - image_base[line][col][dim]
+        return dif
 
     def rename_image(self, num):
         id = str(num)
